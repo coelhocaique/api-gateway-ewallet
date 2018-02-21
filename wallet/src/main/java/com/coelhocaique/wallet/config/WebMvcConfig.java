901637379b1bbaa6340c4faa452cd61a238adecb
Caique Coelho <caique.coelho@maxipago.com>
2018-02-21 10:40:36 -0300

    removing unneeded config

--- wallet/src/main/java/com/coelhocaique/wallet/config/WebMvcConfig.java
+++ wallet/src/main/java/com/coelhocaique/wallet/config/WebMvcConfig.java
@@ -3,7 +3,6 @@ package com.coelhocaique.wallet.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import com.coelhocaique.wallet.config.interceptor.AuthenticationInterceptor;
@@ -13,15 +12,6 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
 	
 	@Autowired
 	private AuthenticationInterceptor authenticationInterceptor;
-	
-	@Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/swagger-ui.html")
-                .addResourceLocations("classpath:/META-INF/resources/");
-
-        registry.addResourceHandler("/webjars/**")
-                .addResourceLocations("classpath:/META-INF/resources/webjars/");
-    }
 	
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
